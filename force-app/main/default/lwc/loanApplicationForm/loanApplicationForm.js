9bae2cad1c8fcc05f407c35e6aa855f2886c9bbf
FortunateC0der <fortunate.mothoana@gmail.com>
2026-01-21 10:56:29 +0200

    update form

--- force-app/main/default/lwc/loanApplicationForm/loanApplicationForm.js
+++ force-app/main/default/lwc/loanApplicationForm/loanApplicationForm.js
@@ -1,30 +1,57 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 
-// Parent-child communication -> Child sends data to parent via (CustomEvent)
-
+// Parent handles step-state and receives child data via CustomEvent
 export default class LoanApplicationForm extends LightningElement {
-    customerInfo = {};   // Store data received from  PersonalDetails child
-    loanInfo = {};   // Store data received from LoanDetails child
+  // Step state
+  isStep1 = true;
+  isStep2 = false;
+  isSuccessful = false;
 
-    // Sends data from personalDetails child to parent on save
-    handlePersonalDetailSave(event) {
-        this.customerInfo = event.detail;
-        console.log('Customer Info Data:', this.customerInfo);
-    }
+  // Data from children
+  customerInfo = {};
+  loanInfo = {};
 
-    // Sends updates to parent from loanDetails child
-    handleLoanDetailSave(event) {
-        this.loanInfo = event.detail;
-        console.log('Loan Info Updated:', this.loanInfo);
-    }
+  // Receives and stores personal details from child
+  handlePersonalDetailSave(event) {
+    this.customerInfo = event.detail;
+    console.log("Customer Info Data:", this.customerInfo);
+  }
 
-    // Final submission of loanApplicationForm
-    submitApplication() {
-        // Combine data from both children
-        const applicationData = {
-            customer: this.customerInfo,
-            loan: this.loanInfo
-        };
-        console.log('This Loan Application:', applicationData, 'has been submitted.');
-    }
+  // Receives and stores loan details from child
+  handleLoanDetailSave(event) {
+    this.loanInfo = event.detail;
+    console.log("Loan Info Updated:", this.loanInfo);
+  }
+
+  // Move from Step 1 -> Step 2
+  handleNextStep() {
+    this.isStep1 = false;
+    this.isStep2 = true;
+  }
+
+  // Move back to Step 1
+  handleBackStep() {
+    this.isStep1 = true;
+    this.isStep2 = false;
+  }
+
+  // Submit application and show confirmation
+  submitApplication() {
+    const applicationData = {
+      customer: this.customerInfo,
+      loan: this.loanInfo
+    };
+    console.log("This Loan Application has been submitted:", applicationData);
+    this.isSuccessful = true;
+    this.isStep2 = false;
+  }
+
+  // Reset form to initial state to submit another application
+  handleResetForm() {
+    this.customerInfo = {};
+    this.loanInfo = {};
+    this.isSuccessful = false;
+    this.isStep1 = true;
+    this.isStep2 = false;
+  }
 }
--- force-app/main/default/lwc/personalDetails/personalDetails.js
+++ force-app/main/default/lwc/personalDetails/personalDetails.js
@@ -1,39 +1,46 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 
 export default class PersonalDetails extends LightningElement {
-    // Hold current input values from user
-    name = '';
-    idNumber = '';
-    phone = '';
+  // Hold current input values from user
+  name = "";
+  idNumber = "";
+  phone = "";
 
-    // Called when name input changes
-    handleNameChange(event) {
-        this.name = event.target.value;
-        this.sendDataToParent();
-    }
+  // Called when name input changes
+  handleNameChange(event) {
+    this.name = event.target.value;
+    this.sendDataToParent();
+  }
 
-    // Called when ID number input changes
-    handleIdChange(event) {
-        this.idNumber = event.target.value;
-        this.sendDataToParent();
-    }
+  // Called when ID number input changes
+  handleIdChange(event) {
+    this.idNumber = event.target.value;
+    this.sendDataToParent();
+  }
 
-    // Called when the phone input changes
-    handlePhoneChange(event) {
-        this.phone = event.target.value;
-        this.sendDataToParent();
-    }
+  // Called when the phone input changes
+  handlePhoneChange(event) {
+    this.phone = event.target.value;
+    this.sendDataToParent();
+  }
 
-    // Sends the current values to the parent component
-    sendDataToParent() {
-        this.dispatchEvent(
-            new CustomEvent('personaldetailsave', {
-                detail: {
-                    name: this.name,
-                    idNumber: this.idNumber,
-                    phone: this.phone
-                }
-            })
-        );
-    }
+  // Sends the current values to the parent component
+  sendDataToParent() {
+    this.dispatchEvent(
+      new CustomEvent("personaldetailsave", {
+        detail: {
+          name: this.name,
+          idNumber: this.idNumber,
+          phone: this.phone
+        }
+      })
+    );
+  }
+
+  // Called when the user clicks Next to proceed to loan details
+  handleNextClick() {
+    // Ensure parent has latest values before moving on
+    this.sendDataToParent();
+    this.dispatchEvent(new CustomEvent("nextstep"));
+  }
 }
